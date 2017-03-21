1525368a0eb57a4880a226df1578f2a661912ae0
novakw267 <novakw267@gmail.com>
2017-03-21 11:28:24 -0400

    Encapsulated list items into a separate componant
    moved code from the listr-list component to listr-list/item
    Tested in the browser

--- app/router.js
+++ app/router.js
@@ -7,7 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('lists');
+  this.route('lists', function() {
+    this.route('Favorites');
+    this.route('Todos');
+  });
 });
 
 export default Router;
