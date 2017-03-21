313bdb89944232a49c0ce9b2757fe6788c47ae90
novakw267 <novakw267@gmail.com>
2017-03-21 12:12:59 -0400

    Added a strike off the list
    I can now click the items in the list and check them off to note that
    I have completed the task.

--- app/components/listr-list.js
+++ app/components/listr-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['listDetailHidden'],
+  listDetailHidden: true,
+  // defining what should happen when the title is clicked on
+  // it says it sould be a hide and show function
+  actions: {
+    toggleListDetail () {
+      return this.toggleProperty('listDetailHidden');
+    }
+  }
 });
--- app/components/listr-list/item.js
+++ app/components/listr-list/item.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['listItemCompleted'],
+  listDetailHidden: true,
+  // defining what should happen when the title is clicked on
+  // it says it sould be a hide and show function
+  actions: {
+    toggleListItem () {
+      return this.toggleProperty('listItemCompleted');
+    }
+  }
 });
